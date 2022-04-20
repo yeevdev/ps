abc5f1817f67c126d5edb2ed5e340967248cafab
YeeV <100995376+yeevdev@users.noreply.github.com>
2022-04-20 17:31:10 +0900

    Update N과 M （1）.cc

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,14 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 #define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-
 
 using namespace std;
 
 
-void backTracking(int n, int m, vector<bool>& visited, vector<int>& v);
+void backTracking(int, int, bool[], vector<int>&);
 
 int main() {
 	FASTIO
@@ -16,26 +13,29 @@ int main() {
 	int N, M;
 	cin >> N >> M;
 
-	vector<bool> visited(N + 1);
+	//방문여부를 저장하는 bool형 visited 배열과 현재 진행상태를 저장하는 벡터 v를 정의
+	bool visited[N + 1] = {false};
 	vector<int> v;
-
+	
 	backTracking(N, M, visited, v);
-
 
 	return 0;
 }
 
-void backTracking(int n, int m, vector<bool>& visited, vector<int>& v) {
+//백트래킹을 재귀함수로 구현
+void backTracking(int n, int m, bool visited[], vector<int>& v) {
+	//벡터에 m개의 원소가 담기면 조건을 만족했으므로 출력하고 종료
 	if(v.size() == m) {
 		for(auto& element : v) {
-			cout << element << " ";
+			cout << element << ' ';
 		}
 
-		cout << "\n";
+		cout << '\n';
 
 		return;
 	}
 
+	//벡터에 m개의 원소가 담기지 않았으므로 m개의 원소가 담길 때까지 재귀
 	for(int i = 1; i <= n; i++) {
 		if(!visited[i]) {
 			visited[i] = true;
